3e6b8960205a88cdce13218dbe31cc4d4c84e8b0
arun <maniarunvenkatesh@gmail.com>
2024-05-25 00:40:04 +0530

    dreams_content

--- frontend/src/Brochere/index.js
+++ frontend/src/Brochere/index.js
@@ -1,10 +1,41 @@
 import React from 'react'
 import './brochere.css'
+import {RiDownloadFill} from "@remixicon/react";
+import './about_us.css'
 
 
 const Brochere = () => {
   return (
     <>
+
+    <div className="about_container">
+        <div className="container">
+            <div className="image_container">   
+                <div className="image"></div>
+            </div>
+            <div className="content_container">
+                <div className="container">
+                    <div className="con_header">Setting World Class Benchmarks of Construction Excellence</div>
+                    <div className="con_subheader">Building Dreams - <span>With Us For You</span></div>
+                
+                    <div className="content1">Senate Engineering was founded by a core team of professionals
+                    with over 20+ years of hands-on experience in constructionandprojectmanagement..</div>
+                    <div className="content2">Senate Engineering was founded in the year 2018 as a core construction company and
+                    quickly transformed into a multi-dimensional construction company with strong expertise in various facets of construction. 
+                    Engineers with 4 to 20 years of handson experience in different facades of the construction industry.</div> 
+                    <div className="download_link"><RiDownloadFill className='download_icon'/>Download our Brochere</div> 
+                </div>              
+            </div>
+        </div>
+
+
+    </div>
+
+
+
+
+
+
         <div className="brochere_container">
 
             <div className="container">
