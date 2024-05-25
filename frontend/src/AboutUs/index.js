f03257c41900aaefd75da776ca59acff43b9b944
arun <maniarunvenkatesh@gmail.com>
2024-05-25 22:26:49 +0530

    about_us

---
+++ frontend/src/AboutUs/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './about.css'
+
+const AboutUs = () => {
+  return (
+    <>
+    
+        <div className="aboutus">
+            <div className="aboutus_image"></div>
+            <div className="aboutus_content">
+                <div className="header">About Us</div>
+                <div className="content">
+                Senate Engineering was founded by a core team of professionals with over 20+ years
+                 of hands-on experience in constructionandprojectmanagement. 
+                 Senate Engineering was founded in the year 2018 as a core construction company and 
+                 quickly transformed into a multi-dimensional construction company with strong expertise 
+                 in various facets of construction. Engineers with 4 to 20 years of handson experience in 
+                 different facades of the construction industry. All our completed projects stand as a tall testimony to our eye for details, construction quality, contemporary design, architectural merits, finesse, standards of materials used, aesthetic values,
+                 top notch amenities, grandeur finish and our passion for excellence
+                </div>
+
+                <div className="contactus_button">Contact Us</div>
+    
+            </div>
+        </div>
+    
+    </>
+  )
+}
+
+export default AboutUs
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,16 @@ import './App.css';
 import HomePage from './HomePage';
 import {Route,Router,Routes} from 'react-router-dom'
 import Services from './OurServices';
+import AboutUs from './AboutUs';
 
 function App() {
   return (
    <>
 
     <Routes>
-
       <Route path='/' element={<HomePage/>}/>
       <Route path='/services' element={<Services/>}/>
+      <Route path='/aboutus' element={<AboutUs/>}/>
 
     </Routes>
 
